c19c0c5b0246fb64b690501a8d8fc2f9f42ca2b2
Andrew Edwards <andrewangeta@gmail.com>
2020-08-07 23:40:44 -0400

    Made properties public.

--- Sources/TelesignKit/Models/Status.swift
+++ Sources/TelesignKit/Models/Status.swift
@@ -9,9 +9,9 @@ import Foundation
 
 public struct Status: TelesignModel {
     /// This code describes the status of your transaction. You can use this code to programmatically respond. You are returned two types of codes, either status codes or error codes.
-    var code: Int
+    public var code: Int
     /// A text description of the status code.
-    var description: String
+    public var description: String
     /// This is a timestamp showing when your transaction status was updated last.
-    var updatedOn: Date?
+    public var updatedOn: Date?
 }
